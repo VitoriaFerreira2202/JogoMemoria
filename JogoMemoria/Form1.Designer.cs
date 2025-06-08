952c82813fdf9d2e73c6ac501da5d7b75bb7da2f
Vitoria Ferreira <vitoria.ferrsenac@gmail.com>
2025-06-08 20:47:24 -0300

    FEAT:TERMINEI!!!

--- JogoMemoria/Form1.Designer.cs
+++ JogoMemoria/Form1.Designer.cs
@@ -287,6 +287,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(535, 511);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
